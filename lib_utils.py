fa84cacf475177ff2a8ea425696aba8c760cf2f3
Jules Gagnon-Marchand <jules.gagnonm.alt@gmail.com>
2024-01-22 16:37:26 -0500

    pre-delete

--- lib_utils.py
+++ lib_utils.py
@@ -1,7 +1,41 @@
 import pathlib
 import h5py
 import logging
+import rich.live
+import rich.table
+
 LOGGER = logging.getLogger(__name__)
+
+class LiveTable:
+    def __init__(self, *columns, table_kwargs=None, **kwargs):
+        if table_kwargs is None:
+            table_kwargs = {}
+        
+        self._columns = columns
+        self._table = rich.table.Table(*columns, **kwargs)
+        self._live_ctx = rich.live.Live(
+            self._table, 
+            refresh_per_second=1,
+            **table_kwargs, 
+            # auto_refresh=False,
+        )
+        self._auto_refresh = True
+
+    def __enter__(self):
+        self._live_ctx.__enter__()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self._live_ctx.refresh()
+        return self._live_ctx.__exit__(*args, **kwargs)
+
+    def add_row(self, **column_dict):
+        self._live_ctx.console.print("Adding row")
+        assert len(column_dict) == len(self._columns), str(
+            (column_dict.keys(), self._columns))
+        reordered_column_dict = [column_dict[key] for key in self._columns]
+        self._table.add_row(*reordered_column_dict)
+        self._live_ctx.update(self._table, refresh=not self._auto_refresh)
 
 
 def logging_basic_config(level=logging.DEBUG):
